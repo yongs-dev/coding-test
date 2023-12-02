dcd215fda895f5b8292af54c0cd8dcf05cf5bc38
Ethan <yongseok993@gmail.com>
2023-12-02 17:30:37 +0900

    introductory test

--- src/test/java/com/mark/codingtest/introductorycourse/IntroductoryTest.java
+++ src/test/java/com/mark/codingtest/introductorycourse/IntroductoryTest.java
@@ -1249,4 +1249,225 @@ public class IntroductoryTest {
 
         assertThat(answer).isEqualTo(1);
     }
+
+    /**
+     * 머쓱이는 큰 종이를 1 x 1 크기로 자르려고 합니다.
+     * 예를 들어 2 x 2 크기의 종이를 1 x 1 크기로 자르려면 최소 가위질 세 번이 필요합니다.
+     * 2	2	3
+     * 2	5	9
+     * 1	1	0
+     */
+    @Test
+    public void 종이_자르기() {
+        int m = 2;
+        int n = 5;
+        int answer = m * n - 1;
+
+        assertThat(answer).isEqualTo(9);
+    }
+
+    /**
+     * up, down, left, right 방향키가 있으며 각 키를 누르면 위, 아래, 왼쪽, 오른쪽으로 한 칸씩 이동합니다.
+     * 예를 들어 [0,0]에서 up을 누른다면 캐릭터의 좌표는 [0, 1],
+     * down을 누른다면 [0, -1], left를 누른다면 [-1, 0], right를 누른다면 [1, 0]입니다.
+     * 머쓱이가 입력한 방향키의 배열 keyinput와 맵의 크기 board이 매개변수로 주어집니다. 캐릭터는 항상 [0,0]에서 시작할 때 키 입력이 모두 끝난 뒤에 캐릭터의 좌표 [x, y]를 return하도록 solution 함수를 완성해주세요.
+     *
+     * ["left", "right", "up", "right", "right"]	[11, 11]	[2, 1]
+     * ["down", "down", "down", "down", "down"]	[7, 9]	[0, -4]
+     */
+    @Test
+    public void 캐릭터의_좌표() {
+        String[] keyinput = {"down", "down", "down", "down", "down"};
+        int[] board = {7, 9};
+        int[] answer = {0, 0};
+        int xLimit = board[0] / 2; // 3
+        int yLimit = board[1] / 2; // 4
+
+        for (String str : keyinput) {
+            if (str.equals("left") && answer[0] > -xLimit) {
+                answer[0]--;
+            } else if (str.equals("right") && answer[0] < xLimit) {
+                answer[0]++;
+            } else if (str.equals("up") && answer[1] < yLimit) {
+                answer[1]++;
+            } else if (str.equals("down") && answer[1] > -yLimit) {
+                answer[1]--;
+            }
+        }
+
+        assertThat(answer).containsExactly(0, -4);
+    }
+
+    /**
+     * 직사각형 네 꼭짓점의 좌표 [[x1, y1], [x2, y2], [x3, y3], [x4, y4]]가 담겨있는 배열 dots가 매개변수로 주어질 때,
+     * 직사각형의 넓이를 return 하도록 solution 함수를 완성해보세요.
+     *
+     * [[1, 1], [2, 1], [2, 2], [1, 2]]	    1
+     * [[-1, -1], [1, 1], [1, -1], [-1, 1]]	4
+     */
+    @Test
+    public void 직사각형_넓이_구하기() {
+        int[][] dots = new int[][]{{-1, -1}, {1, 1}, {1, -1}, {-1, 1}};
+        int x = dots[0][0];
+        int y = dots[0][1];
+        int width = 0;
+        int height = 0;
+
+        for (int[] dot : dots) {
+            if (x != dot[0]) {
+                width = Math.abs(x - dot[0]);
+            }
+
+            if (y != dot[1]) {
+                height = Math.abs(y - dot[1]);
+            }
+        }
+
+        int answer = width * height;
+
+        assertThat(answer).isEqualTo(4);
+    }
+
+    /**
+     * 머쓱이가 입력한 아이디와 패스워드가 담긴 배열 id_pw와 회원들의 정보가 담긴 2차원 배열 db가 주어질 때,
+     * 다음과 같이 로그인 성공, 실패에 따른 메시지를 return하도록 solution 함수를 완성해주세요.
+     * 아이디와 비밀번호가 모두 일치하는 회원정보가 있으면 "login"을 return합니다.
+     * 로그인이 실패했을 때 아이디가 일치하는 회원이 없다면 “fail”를, 아이디는 일치하지만 비밀번호가 일치하는 회원이 없다면 “wrong pw”를 return 합니다.
+     * ["meosseugi", "1234"]	    [["rardss", "123"], ["yyoom", "1234"], ["meosseugi", "1234"]]	                "login"
+     * ["programmer01", "15789"]	[["programmer02", "111111"], ["programmer00", "134"], ["programmer01", "1145"]]	"wrong pw"
+     * ["rabbit04", "98761"]	    [["jaja11", "98761"], ["krong0313", "29440"], ["rabbit00", "111333"]]	        "fail"
+     */
+    @Test
+    public void 로그인_성공() {
+        String[] id_pw = new String[]{"meosseugi", "1234"};
+        String[][] db = new String[][]{{"rardss", "123"}, {"yyoom", "1234"},{"meosseugi", "1234"}};
+        String answer = "fail";
+
+        for (String[] data : db) {
+            if (data[0].equals(id_pw[0]) && data[1].equals(id_pw[1])) {
+                answer = "login";
+                break;
+            } else if (data[0].equals(id_pw[0]) && !data[1].equals(id_pw[1])) {
+                answer = "wrong pw";
+                break;
+            }
+        }
+
+        assertThat(answer).isEqualTo("login");
+    }
+
+    /**
+     * 영어 점수와 수학 점수의 평균 점수를 기준으로 학생들의 등수를 매기려고 합니다. 영어 점수와 수학 점수를 담은 2차원 정수 배열 score가 주어질 때,
+     * 영어 점수와 수학 점수의 평균을 기준으로 매긴 등수를 담은 배열을 return하도록 solution 함수를 완성해주세요.
+     * [[80, 70], [90, 50], [40, 70], [50, 80]]	                                    [1, 2, 4, 3]
+     * [[80, 70], [70, 80], [30, 50], [90, 100], [100, 90], [100, 100], [10, 30]]	[4, 4, 6, 2, 2, 1, 7]
+     */
+    @Test
+    public void 등수_매기기() {
+        int[][] score = {{80, 70}, {70, 80}, {30, 50}, {90, 100}, {100, 90}, {100, 100}, {10, 30}};
+        int[] average = new int[score.length];
+        int[] answer = new int[score.length];
+
+        for (int i=0; i<score.length; i++) {
+            average[i] = (score[i][0] + score[i][1]) / 2;
+        }
+
+        for (int i=0; i<average.length; i++) {
+            int order = 1;
+            for (int j=0; j<average.length; j++) {
+                if (average[i] < average[j]) {
+                    order++;
+                }
+                answer[i] = order;
+            }
+        }
+
+        assertThat(answer).containsExactly(4, 4, 6, 2, 2, 1, 7);
+    }
+
+    /**
+     * 프로그래머스 치킨은 치킨을 시켜먹으면 한 마리당 쿠폰을 한 장 발급합니다. 쿠폰을 열 장 모으면 치킨을 한 마리 서비스로 받을 수 있고,
+     * 서비스 치킨에도 쿠폰이 발급됩니다. 시켜먹은 치킨의 수 chicken이 매개변수로 주어질 때 받을 수 있는 최대 서비스 치킨의 수를 return하도록 solution 함수를 완성해주세요.
+     * 100	    11
+     * 1,081	120
+     */
+    @Test
+    public void 치킨_쿠폰() {
+        int chicken = 1081;
+        int bonus = 10;
+        int answer = 0;
+
+        while (chicken >= bonus) {
+            int newChick = chicken / bonus;
+            int restChick = chicken % bonus;
+            chicken = newChick + restChick;
+            answer += newChick;
+        }
+
+        assertThat(answer).isEqualTo(120);
+    }
+
+    /**
+     * 소수점 아래 숫자가 계속되지 않고 유한개인 소수를 유한소수라고 합니다. 분수를 소수로 고칠 때 유한소수로 나타낼 수 있는 분수인지 판별하려고 합니다.
+     * 기약분수로 나타내었을 때, 분모의 소인수가 2와 5만 존재해야 합니다.
+     * 두 정수 a와 b가 매개변수로 주어질 때, a/b가 유한소수이면 1을, 무한소수라면 2를 return하도록 solution 함수를 완성해주세요.
+     * 7	20	1
+     * 11	22	1
+     * 12	21	2
+     */
+    @Test
+    public void 유한소수_판별() {
+        int a = 11;
+        int b = 22;
+        int answer = 2;
+
+        // 최대 공약수 구하기
+        int gcd = 1;
+        for (int i = 2; i <= Math.min(a, b); i++) {
+            if (a % i == 0 && b % i == 0) {
+                gcd = i;
+            }
+        }
+        int newA = a / gcd;
+        int newB = b / gcd;
+
+        while (newB % 2 == 0 && newB != 1) {
+            newB = newB / 2;
+        }
+
+        while (newB % 5 == 0 && newB != 1) {
+            newB = newB / 5;
+        }
+
+        if (newB == 1) {
+            answer = 1;
+        }
+
+        assertThat(answer).isEqualTo(1);
+    }
+
+    /**
+     * 3x 마을 사람들은 3을 저주의 숫자라고 생각하기 때문에 3의 배수와 숫자 3을 사용하지 않습니다. 3x 마을 사람들의 숫자는 다음과 같습니다.
+     * 10진법	3x 마을에서 쓰는 숫자	10진법	3x 마을에서 쓰는 숫자
+     * 1	    1	                6	    8
+     * 2	    2	                7	    10
+     * 3	    4	                8	    11
+     * 4	    5	                9	    14
+     * 5	    7	                10  	16
+     *
+     */
+    @Test
+    public void 저주의_숫자_3() {
+        int n = 15;
+        int answer = 0;
+
+        for (int i=1; i<=n; i++) {
+            if (String.valueOf(i).contains("3") || i % 3 == 0) {
+                n++;
+            }
+        }
+
+        answer = n;
+        assertThat(answer).isEqualTo(25);
+    }
 }
